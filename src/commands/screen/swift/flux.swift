8f4a18563a57e13636802e4fe81297b338df9e53
Christophe Geers <geersch@gmail.com>
2026-05-02 22:48:05 +0200

    fix: apply warm gamma table to all active displays

--- src/commands/screen/swift/flux.swift
+++ src/commands/screen/swift/flux.swift
@@ -12,28 +12,33 @@ guard args.count == 2, args[1] == "enable" || args[1] == "disable" else {
     exit(1)
 }
 
-let displayID = CGMainDisplayID()
-
 if args[1] == "disable" {
     CGDisplayRestoreColorSyncSettings()
     exit(0)
 }
 
-// enable: apply warm gamma table and hold it
-let cap = Int(CGDisplayGammaTableCapacity(displayID))
+// enable: enumerate all active displays, apply warm gamma table to each, and hold it
+var displayCount: UInt32 = 0
+CGGetActiveDisplayList(0, nil, &displayCount)
+var displayIDs = [CGDirectDisplayID](repeating: 0, count: Int(displayCount))
+CGGetActiveDisplayList(displayCount, &displayIDs, &displayCount)
 
-var redTable   = [CGGammaValue](repeating: 0, count: cap)
-var greenTable = [CGGammaValue](repeating: 0, count: cap)
-var blueTable  = [CGGammaValue](repeating: 0, count: cap)
+for displayID in displayIDs {
+    let cap = Int(CGDisplayGammaTableCapacity(displayID))
 
-for i in 0..<cap {
-    let v = CGGammaValue(i) / CGGammaValue(cap - 1)
-    redTable[i]   = v * 1.0
-    greenTable[i] = v * 0.76902770996094
-    blueTable[i]  = v * 0.5240478515625
+    var redTable   = [CGGammaValue](repeating: 0, count: cap)
+    var greenTable = [CGGammaValue](repeating: 0, count: cap)
+    var blueTable  = [CGGammaValue](repeating: 0, count: cap)
+
+    for i in 0..<cap {
+        let v = CGGammaValue(i) / CGGammaValue(cap - 1)
+        redTable[i]   = v * 1.0
+        greenTable[i] = v * 0.76902770996094
+        blueTable[i]  = v * 0.5240478515625
+    }
+
+    CGSetDisplayTransferByTable(displayID, UInt32(cap), &redTable, &greenTable, &blueTable)
 }
-
-CGSetDisplayTransferByTable(displayID, UInt32(cap), &redTable, &greenTable, &blueTable)
 
 // Stay alive to hold the gamma table
 dispatchMain()
